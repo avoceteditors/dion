b66bc401489b0dc9302606d759a9996a24ee3d09
Kenneth P. J. Dyer <kenneth.dyer@mariadb.com>
2020-02-17 16:48:39 -0500

    Reworking for Python

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+import pathlib
+import re
+
+# Configure Package Names
+packages = [
+    "dion",
+]
+package_dirs = {}
+
+for package in packages:
+    package_dirs[package] = re.sub("\.", "/", package)
+
+# Configure Scripts
+scripts_dir = pathlib.Path("scripts")
+scripts =[]
+
+for i in scripts_dir.glob("*"):
+    scripts.append(str(i))
+
+
+# Initialize Library
+setup(
+    name="dion",
+    version="2020.1",
+    packages=packages,
+    package_dir=package_dirs,
+    scripts=scripts,
+    package_data={'dion':['data/Makefile', 'data/sql/*.sql']} 
+)
