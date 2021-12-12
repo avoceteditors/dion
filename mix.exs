429257054c8bf98f645aa003423008f825c67caf
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2021-12-12 16:46:51 -0500

    Increments version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dion.MixProject do
   def project do
     [
       app: :dion,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       escript: [main_module: Dion.CLI],
       start_permanent: Mix.env() == :prod,
